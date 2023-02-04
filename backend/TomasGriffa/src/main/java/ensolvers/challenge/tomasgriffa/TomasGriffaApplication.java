9d61ce4d3381ff057d77370cdf0c593aefef5a11
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2023-02-04 20:04:28 -0300

    elimine las notas

--- backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
+++ backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
@@ -14,18 +14,4 @@ public class TomasGriffaApplication {
     public static void main(String[] args) {
         SpringApplication.run(TomasGriffaApplication.class, args);
     }
-
-    @Autowired
-    private NoteRepository noteRepository;
-
-    @Bean
-    public CommandLineRunner init() {
-        return args -> {
-            Note note1 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
-            Note note2 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
-            Note note3 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
-            Note note4 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
-
-        };
-    }
 }
