e01cf29d25191771d901da4f6b132e6bb9e887db
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2023-02-04 19:58:51 -0300

    agregue un par de notas a ver si se muestran

--- backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
+++ backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
@@ -1,7 +1,12 @@
 package ensolvers.challenge.tomasgriffa;
 
+import ensolvers.challenge.tomasgriffa.model.Note;
+import ensolvers.challenge.tomasgriffa.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TomasGriffaApplication {
@@ -10,4 +15,17 @@ public class TomasGriffaApplication {
         SpringApplication.run(TomasGriffaApplication.class, args);
     }
 
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Bean
+    public CommandLineRunner init() {
+        return args -> {
+            Note note1 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
+            Note note2 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
+            Note note3 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
+            Note note4 = noteRepository.save(new Note("titulo 1", "cuerpo 1"));
+
+        };
+    }
 }
