a0c76d0daffdb0ccc58e6a6a41a620eea0e7abe6
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2023-02-05 15:29:12 -0300

    dejo todo en update

--- backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
+++ backend/TomasGriffa/src/main/java/ensolvers/challenge/tomasgriffa/TomasGriffaApplication.java
@@ -17,23 +17,23 @@ public class TomasGriffaApplication {
         SpringApplication.run(TomasGriffaApplication.class, args);
     }
 
-    @Autowired
-    private NoteRepository noteRepository;
-    @Autowired
-    private UserRepository userRepository;
-
-    @Bean
-    public CommandLineRunner init() {
-        return args -> {
-
-            Note note1 = noteRepository.save(new Note("Titulo 1", "nota 1"));
-            Note note2 = noteRepository.save(new Note("Titulo 2", "nota 2"));
-
-            User user = new User("admin@gmail.com", "1234");
-            user.addNote(note1);
-            user.addNote(note2);
-            userRepository.save(user);
-
-        };
-    }
+//    @Autowired
+//    private NoteRepository noteRepository;
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Bean
+//    public CommandLineRunner init() {
+//        return args -> {
+//
+//            Note note1 = noteRepository.save(new Note("Titulo 1", "nota 1"));
+//            Note note2 = noteRepository.save(new Note("Titulo 2", "nota 2"));
+//
+//            User user = new User("admin@gmail.com", "1234");
+//            user.addNote(note1);
+//            user.addNote(note2);
+//            userRepository.save(user);
+//
+//        };
+//    }
 }
